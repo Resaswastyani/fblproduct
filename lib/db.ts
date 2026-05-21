6534e5877c81ead35a57f25dcdc4fccc6e8928b7
Resaswastyani <resaarrazy@gmail.com>
2026-05-21 19:56:35 +0700

    perbaikan

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 
-if (!process.env.DATABASE_URL) {
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-export const sql = neon(process.env.DATABASE_URL);
+export const sql = neon(connectionString);
